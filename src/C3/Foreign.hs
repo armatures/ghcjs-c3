2563f7c8a120363036ddc034ee4e5dfe6d331b8b
Michael Beidler <michael.beidler@gmail.com>
2016-11-05 00:24:31 -0700

    formatting changes

--- src/C3/Foreign.hs
+++ src/C3/Foreign.hs
@@ -5,12 +5,10 @@ import Data.JSString (JSString)
 import GHCJS.Types   (JSVal)
 import C3.Types
 
-foreign import javascript unsafe
-  "$r = c3.generate($1);" 
+foreign import javascript unsafe "$r = c3.generate($1);" 
   js_c3_generate :: JSVal -> IO Chart
 
-foreign import javascript unsafe
-  "$1.transform($2);"
+foreign import javascript unsafe "$1.transform($2);"
   js_c3_transform :: Chart -> JSVal -> IO ()
 
 foreign import javascript unsafe
