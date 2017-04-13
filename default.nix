ddecd55665ab687962df7fd75d11c7a2bd3c791f
brady.ouren <brady.ouren@gmail.com>
2017-04-12 20:44:36 -0700

    config as ghcjs

--- default.nix
+++ default.nix
@@ -1,21 +1,17 @@
-{ mkDerivation
-, aeson
-, base
-, data-default
-, ghcjs-base ? null
-, stdenv
-, text
-, time
-, vector
+{ mkDerivation, aeson, base, data-default, ghcjs-base, stdenv, text
+, time, vector
 }:
 mkDerivation {
   pname = "ghcjs-c3";
   version = "0.0.0.1";
-  src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
+  src = ./.;
   isLibrary = true;
+  isExecutable = true;
   libraryHaskellDepends = [
     aeson base data-default ghcjs-base text time vector
   ];
+  executableHaskellDepends = [ base data-default ];
+  testHaskellDepends = [ base ];
   homepage = "https://github.com/mbeidler/ghcjs-c3#readme";
   description = "C3 bindings for GHCJS";
   license = stdenv.lib.licenses.bsd3;
